e958c86641c780fd721a2a1f88b470247fe39a36
David Molina Gámez <dmg00068@red.ujaen.es>
2025-04-11 13:51:10 +0200

    Configuraciones iniciales

---
+++ src/main/java/ujadeportes/UjadeportesApplication.java
@@ -0,0 +1,12 @@
+package ujadeportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjadeportesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UjadeportesApplication.class, args);
+    }
+}
